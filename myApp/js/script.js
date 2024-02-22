1db74c313b85d75eb0afd7750fcff9965e84bc3e
MarcoFonti <theemporiumg@gmail.com>
2024-02-22 16:46:41 +0100

    add mold request API

--- myApp/js/script.js
+++ myApp/js/script.js
@@ -1,7 +1,23 @@
 const {createApp} = Vue;
 
+// VARIABILE URL API
+const endpoint = 'http://localhost/boolean/php-todo-list-json/api/task/';
+
 const app = createApp ({
 
+    name: 'MyApp',
+
+    // DATI
+    data: () => ({
+        tasks: []
+    }),
+
+    // ALL'AVVIO DELLA PAGINA
+    created(){
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data;
+        })
+    }
   
 })
 
