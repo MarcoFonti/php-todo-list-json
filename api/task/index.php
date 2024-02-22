5d06d562077b9866c01d6bdde171e8db50d0f9a5
MarcoFonti <theemporiumg@gmail.com>
2024-02-22 17:21:35 +0100

    error line 6 index.php

--- api/task/index.php
+++ api/task/index.php
@@ -1,28 +1,16 @@
 <?php
 
-// MODIFICO ARRAY DI OGGETTI FORNITO IN UN ARRAY ASSOCIATIVO
-$tasks = [
-    [
-        "id" => 1,
-        "text" => "HTML",
-        "done" => false
-    ],
-    [
-        "id" => 2,
-        "text" => "CSS",
-        "done" => false
-    ],
-    [
-        "id" => 3,
-        "text" => "Responsive design",
-        "done" => false
-    ],
-    [
-        "id" => 4,
-        "text" => "Javascript",
-        "done" => false
-    ]
-];
+$path = __DIR__ . '/database/task.json';
+
+// LEGGO DAL FILE TASK.JSON I DATI FORNITI
+$data = file_get_contents($path);
+
+// CONVERTO LO STRINGONE IN JSON
+$tasks = json_decode($data, true);
+
+var_dump($tasks);
+
+die();
 
 // RESTITUISCO UN FORMATO JSON
 header('Content-Type: application/json');
